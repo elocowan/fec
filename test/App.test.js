1a2f00f6c8117dad99cb1653cfa9b888268c7927
Ethan Cowan <ethan.cowan@gmail.com>
2020-08-06 21:56:10 -0600

    testing circleci

--- test/App.test.js
+++ test/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import App from '../src/components/App';
+import App from '../src/components/app';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
